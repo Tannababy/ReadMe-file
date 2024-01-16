c85238f2a9963bbfac0cad4553f8312d88be9c80
Lotanna <tannaloamobi@gmail.com>
2024-01-16 21:54:29 +0000

    added more questions within quesions array

--- utils/index.js
+++ utils/index.js
@@ -6,6 +6,17 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
   {
+    name: "github",
+    type: "input",
+    message: "What is your GitHub username? (No @ needed)",
+    default: "tannababy",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "What is your email address?",
+  },
+  {
     name: "title",
     type: "input",
     message: "What is the the title of your project?",
@@ -24,24 +35,48 @@ const questions = [
     name: "installation ",
     type: "input",
     message:
-      "Fill out Installation instructions if applicaple, if not type N/A",
+      "If applicable, describe the steps required to install your project for the Installation section, if not type N/A",
   },
   {
-    name: "Usage",
+    name: "usage",
     type: "input",
-    message: "What is the Usage of this project?",
+    message:
+      "Provide instructions and examples of your project in use for the Usage section.",
+  },
+  {
+    name: "contributing",
+    type: "input",
+    message:
+      "If applicable, provide guidelines on how other developers can contribute to your project.",
+  },
+  {
+    name: "tests",
+    type: "input",
+    message:
+      "If applicable, provide any tests written for your application and provide examples on how to run them.",
+  },
+  {
+    name: "questions",
+    type: "input",
+    message: "Create a Questions section here by typing 'questions'",
   },
   {
     name: "licence",
     type: "list",
     message: "Select a licence",
-    choices: ["Mit", ""],
+    choices: [
+      "'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The ",
+    ],
   },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {}
+const file = ``;
 
+fs.writeFile("NewReadMe.MD", markdown, (err) =>
+  err ? console.error(err) : console.log(`Success!`)
+);
 // function to initialize program
 function init() {}
 
