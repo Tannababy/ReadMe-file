55b467db00dc9c1b07292e5bccd86cf1c526764a
Lotanna <tannaloamobi@gmail.com>
2024-01-16 21:15:02 +0000

    added questions to index.js file

--- utils/index.js
+++ utils/index.js
@@ -4,7 +4,40 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    name: "title",
+    type: "input",
+    message: "What is the the title of your project?",
+  },
+  {
+    name: "description",
+    type: "input",
+    message: "What is the the Description of your project?",
+  },
+  {
+    name: "tableOfContents",
+    type: "input",
+    message: "Fill in Table of Contents",
+  },
+  {
+    name: "installation ",
+    type: "input",
+    message:
+      "Fill out Installation instructions if applicaple, if not type N/A",
+  },
+  {
+    name: "Usage",
+    type: "input",
+    message: "What is the Usage of this project?",
+  },
+  {
+    name: "licence",
+    type: "list",
+    message: "Select a licence",
+    choices: ["Mit", ""],
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
